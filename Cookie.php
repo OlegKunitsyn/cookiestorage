987ba14897e86c86211d8f5ed67e95fb5938adca
Oleg Kunitsyn <olegs.kunicins@gmail.com>
2013-01-17 19:40:36 +0200

    Init

---
+++ Cookie.php
@@ -0,0 +1,222 @@
+<?php
+/**
+ * Auth Cookie Storage for Zend Framework 1.x
+ *
+ * @category   Zend
+ * @package    Zend_Auth
+ * @subpackage Zend_Auth_Storage
+ * @copyright  Oleg Kunitsyn (https://github.com/OlegKunitsyn)
+ * @license    Apache License
+ * @version    $Rev 1.0$
+ */
+
+/**
+ * @see Zend_Auth_Storage_Interface
+ */
+require_once 'Zend/Auth/Storage/Interface.php';
+
+/**
+ * 
+ * 
+ * @category   Zend
+ * @package    Zend_Auth
+ * @subpackage Zend_Auth_Storage
+ * @copyright  Oleg Kunitsyn (https://github.com/OlegKunitsyn)
+ * @license    Apache License
+ */
+class Auth_Storage_Cookie implements Zend_Auth_Storage_Interface
+{
+    /**
+     * Default name of the cookie for the public key
+     */
+    const KEY_COOKIE_NAME_DEFAULT = '_k';
+
+    /**
+     * Default name of the cookie for the value
+     */
+    const VALUE_COOKIE_NAME_DEFAULT = '_v';
+    
+    /**
+     * Default cipher
+     */
+    const CIPHER_DEFAULT = MCRYPT_RIJNDAEL_128;
+    
+    /**
+     * Decrypted cookies
+     * 
+     * @var mixed
+     */
+    protected $_cookie = null;
+
+    /**
+     * Public key
+     * 
+     * @var string
+     */
+    protected $_publicKey = null;
+
+    /**
+     * Secret key
+     * 
+     * @var string
+     */
+    protected $_secretKey = null;
+
+    /**
+     * Name of the cookie for the public key
+     *
+     * @var string
+     */
+    protected $_keyCookieName = null;
+
+    /**
+     * Name of the cookie for the value
+     *
+     * @var string
+     */
+    protected $_valueCookieName = null;
+    
+    /**
+     * Name of the cookie for the value
+     *
+     * @var string
+     */
+    protected $_cipher = null;
+    
+    /**
+     * Initializes storage
+     *
+     * @param  string $secretKey
+     * @param  string $keyCookieName
+     * @param  string $valueCookieName
+     * @return void
+     */
+    public function __construct($secretKey, 
+        $keyCookieName = self::KEY_COOKIE_NAME_DEFAULT,
+        $valueCookieName = self::VALUE_COOKIE_NAME_DEFAULT,
+        $cipher = self::CIPHER_DEFAULT)
+    {
+        $this->_secretKey = $secretKey;
+        $this->_keyCookieName = $keyCookieName;
+        $this->_valueCookieName = $valueCookieName;
+        $this->_cipher = $cipher;
+
+        // Retrieve public key
+        if (!empty($_COOKIE[$this->_keyCookieName])) {
+            try {
+                $this->_publicKey = base64_decode($_COOKIE[$this->_keyCookieName]);
+            } catch (Exception $exception) {
+                $this->_publicKey = null;
+                require_once 'Zend/Auth/Storage/Exception.php';
+                throw new Zend_Auth_Storage_Exception($exception);
+            }
+        }
+        
+        // Setup public key
+        if (empty($this->_publicKey)) {
+            $this->_publicKey = mcrypt_create_iv(
+                mcrypt_get_iv_size($this->_cipher, MCRYPT_MODE_CBC),
+                MCRYPT_RAND
+            );
+            $_COOKIE[$this->_keyCookieName] = base64_encode($this->_publicKey);
+            @setcookie($this->_keyCookieName, base64_encode($this->_publicKey), 0, '/', '', false, true);
+        }
+        
+        // Retrieve data
+        if (empty($_COOKIE[$this->_valueCookieName]))
+            return;
+        try {
+            $this->_cookie = $this->_decrypt($_COOKIE[$this->_valueCookieName]);
+        } catch (Exception $exception) {
+            $this->_cookie = null;
+            require_once 'Zend/Auth/Storage/Exception.php';
+            throw new Zend_Auth_Storage_Exception($exception);
+        }
+    }
+    /**
+     * Encrypt the string
+     *
+     * @param string $string
+     * @return string
+     */
+    protected function _encrypt($string)
+    {
+        $string = serialize($string);
+        $blockSize = mcrypt_get_block_size($this->_cipher, MCRYPT_MODE_CBC);
+        $pad = $blockSize - (strlen($string) % $blockSize);
+        $string .= str_repeat(chr($pad), $pad);
+        $result = mcrypt_encrypt(
+            $this->_cipher,
+            $this->_secretKey,
+            $string,
+            MCRYPT_MODE_CBC,
+            $this->_publicKey
+        );
+        return base64_encode(gzcompress($result, 6));
+    }
+    /**
+     * Decrypt the string
+     *
+     * @param string $string
+     * @return string
+     */
+    protected function _decrypt($string)
+    {
+        $result = mcrypt_decrypt(
+            $this->_cipher,
+            $this->_secretKey,
+            gzuncompress(base64_decode($string)),
+            MCRYPT_MODE_CBC,
+            $this->_publicKey
+        );
+        $pad = ord($result[strlen($result) - 1]);
+        $result = substr($result, 0, strlen($result) - $pad);
+        return unserialize($result);
+    }
+    /**
+     * Defined by Zend_Auth_Storage_Interface
+     *
+     * @return boolean
+     */
+    public function isEmpty()
+    {
+        return empty($this->_cookie);
+    }
+    /**
+     * Defined by Zend_Auth_Storage_Interface
+     *
+     * @return mixed
+     */
+    public function read()
+    {
+        return $this->_cookie;
+    }
+    /**
+     * Defined by Zend_Auth_Storage_Interface
+     *
+     * @param  mixed $contents
+     * @return void
+     */
+    public function write($contents)
+    {
+        try {
+            $encryptedContents = $this->_encrypt($contents);
+        } catch (Exception $e) {
+            $encryptedContents = $contents = null;
+        }
+        $this->_cookie = $contents;
+        $_COOKIE[$this->_valueCookieName] = $encryptedContents;
+        @setcookie($this->_valueCookieName, $encryptedContents, 0, '/', '', false, true);
+    }
+    /**
+     * Defined by Zend_Auth_Storage_Interface
+     *
+     * @return void
+     */
+    public function clear()
+    {
+        $this->_cookie = null;
+        unset($_COOKIE[$this->_valueCookieName]);
+        @setcookie($this->_valueCookieName, '', 1, '/', '', false, true);
+    }
+}
---
+++ demo/digest.php
@@ -0,0 +1,36 @@
+<?php
+require_once 'Zend/Auth.php';
+require_once 'Zend/Auth/Adapter/Digest.php';
+require_once dirname(__FILE__) . '/../Cookie.php';
+
+// map IP to the secret key in order to prevent cookie hijack
+$secret = 'my secret key' . $_SERVER['REMOTE_ADDR'];
+// set up cookie storage
+$auth = Zend_Auth::getInstance()->setStorage(new Auth_Storage_Cookie($secret));
+
+// Logged in?
+if ($auth->hasIdentity()) {
+    $username = $auth->getStorage()->read();
+    Zend_Auth::getInstance()->clearIdentity();
+    echo "I remember you, {$username}!.. Goodbye!.. Please reload the page";
+    exit;
+}
+
+// Do login
+$username = 'someUser';
+$password = 'somePassword';
+$realm = 'Some Realm';
+$filename = 'digest.txt';
+
+$adapter = new Zend_Auth_Adapter_Digest($filename, $realm, $username, $password);
+$result = $auth->authenticate($adapter);
+if ($result->isValid()) {
+    $auth->getStorage()->write($username);
+    echo "Welcome, {$username}!.. Please reload the page";
+} else {
+    echo "Failed login";
+    foreach ($auth->getMessages() as $message)
+        echo "$message\n";
+}
+
+
